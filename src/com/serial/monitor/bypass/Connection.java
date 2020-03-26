aba69c4021bc883671a8d41ff859e533daef14ff
renato <renato.bittencourt13@gmail.com>
2020-03-26 09:11:12 -0300

    serial watcher - hex output

---
+++ src/com/serial/monitor/bypass/Connection.java
@@ -0,0 +1,221 @@
+package com.serial.monitor.bypass;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+import gnu.io.UnsupportedCommOperationException;
+
+public class Connection {
+	private static Connection instance;
+	private String serial1Name = "";
+	private String serial2Name = "";
+
+	private Connection() {
+
+	}
+
+	public static Connection getInstance() {
+		if (instance == null)
+			instance = new Connection();
+		return instance;
+	}
+
+	public String getSerial1Name() {
+		return serial1Name;
+	}
+
+	public void setSerial1Name(String serial1Name) {
+		this.serial1Name = serial1Name;
+	}
+
+	public String getSerial2Name() {
+		return serial2Name;
+	}
+
+	public void setSerial2Name(String serial2Name) {
+		this.serial2Name = serial2Name;
+	}
+
+	public void ConnectPorts(String port1, String port2, String watcher)
+			throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException {
+		CommPortIdentifier port1Identifier = CommPortIdentifier.getPortIdentifier(port1);
+		CommPortIdentifier port2Identifier = CommPortIdentifier.getPortIdentifier(port2);
+		CommPortIdentifier watcherIdentifier = CommPortIdentifier.getPortIdentifier(watcher);
+		if (port1Identifier.isCurrentlyOwned() || port2Identifier.isCurrentlyOwned()
+				|| watcherIdentifier.isCurrentlyOwned()) {
+			System.out.println("Error: Port is currently in use");
+		} else {
+			CommPort commPort1 = port1Identifier.open(this.getClass().getName(), 2000);
+			CommPort commPort2 = port2Identifier.open(this.getClass().getName(), 2000);
+			CommPort commWatcher = watcherIdentifier.open(this.getClass().getName(), 2000);
+
+			SerialPort serialPort1 = (SerialPort) commPort1;
+			serialPort1.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+			InputStream in1 = serialPort1.getInputStream();
+			OutputStream out1 = serialPort1.getOutputStream();
+
+			SerialPort serialPort2 = (SerialPort) commPort2;
+			serialPort2.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+			InputStream in2 = serialPort2.getInputStream();
+			OutputStream out2 = serialPort2.getOutputStream();
+
+			SerialPort serialWatcher = (SerialPort) commWatcher;
+			serialWatcher.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+			// InputStream inw = serialWatcher.getInputStream();
+			OutputStream outw = serialWatcher.getOutputStream();
+
+			setSerial1Name(serialPort1.getName());
+			setSerial2Name(serialPort2.getName());
+
+			(new Thread(new SerialReader(in1, out2, outw, 1))).start();
+			(new Thread(new SerialReader(in2, out1, outw, 2))).start();
+
+		}
+	}
+
+	public static class SerialReader implements Runnable {
+		InputStream in;
+		OutputStream outOtherPort;
+		OutputStream outWatcher;
+		int direction = -1;
+		static int lastDir = -1;
+
+		public SerialReader(InputStream in) {
+			this.in = in;
+		}
+
+		public SerialReader(InputStream in, OutputStream outWatcher) {
+			this.in = in;
+			this.outWatcher = outWatcher;
+
+		}
+
+		public SerialReader(InputStream in, OutputStream outOtherPort, OutputStream outWatcher, int dir) {
+			this.in = in;
+			this.outWatcher = outWatcher;
+			this.outOtherPort = outOtherPort;
+			this.direction = dir;
+		}
+
+		private static String charToHex(char item) {
+			String ret = "";
+			char hi;
+			char lo;
+
+			hi = (char) (item >> 4 & 0x0f);
+			lo = (char) (item & 0x0f);
+
+			ret += nibbleToHex(hi);
+			ret += nibbleToHex(lo);
+			return ret;
+		}
+
+		private static char nibbleToHex(char nibble) {
+			// return (char) (nibble + 0x30);
+			switch (nibble) {
+			case 0x0:
+				return '0';
+			case 0x1:
+				return '1';
+			case 0x2:
+				return '2';
+			case 0x3:
+				return '3';
+			case 0x4:
+				return '4';
+			case 0x5:
+				return '5';
+			case 0x6:
+				return '6';
+			case 0x7:
+				return '7';
+			case 0x8:
+				return '8';
+			case 0x9:
+				return '9';
+			case 0xa:
+				return 'A';
+			case 0xb:
+				return 'B';
+			case 0xc:
+				return 'C';
+			case 0xd:
+				return 'D';
+			case 0xe:
+				return 'E';
+			case 0xf:
+				return 'F';
+			default:
+				return '*';
+			}
+		}
+
+		public void run() {
+			String stamp = "";
+			byte[] buffer = new byte[1024];
+			int len = -1;
+			try {
+
+				while ((len = this.in.read(buffer)) > -1) {
+					synchronized (this.outWatcher) {
+						if (len > 0) {
+							if (direction != lastDir) {
+								if (direction == 1) {
+									stamp = "\n\r[" + Connection.getInstance().getSerial1Name().replace("//./", "")
+											+ "]";
+								} else {
+									stamp = "\n\r[" + Connection.getInstance().getSerial2Name().replace("//./", "")
+											+ "]";
+								}
+								for (char stp : stamp.toCharArray()) {
+									this.outWatcher.write(stp);
+								}
+
+								lastDir = direction;
+							}
+							int c = 0;
+							while (c < len) {
+								String hex = charToHex((char) buffer[c]);
+								this.outWatcher.write(hex.getBytes()[0]);
+								this.outWatcher.write(hex.getBytes()[1]);
+								this.outWatcher.write(' ');
+								this.outOtherPort.write(buffer[c++]);
+							}
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/** */
+	public static class SerialWriter implements Runnable {
+		OutputStream out;
+
+		public SerialWriter(OutputStream out) {
+			this.out = out;
+		}
+
+		public void run() {
+			try {
+				int c = 0;
+				while ((c = System.in.read()) > -1) {
+					this.out.write(c);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/com/serial/monitor/bypass/MainWindow.java
@@ -0,0 +1,125 @@
+package com.serial.monitor.bypass;
+
+import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.UnsupportedCommOperationException;
+
+import javax.swing.JFrame;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+
+public class MainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 341, 185);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JComboBox<String> serial1ComboBox = new JComboBox<String>(getSerialPorts());
+		//JComboBox<String> serial1ComboBox = new JComboBox<String>();
+		serial1ComboBox.setBounds(12, 23, 91, 22);
+		frame.getContentPane().add(serial1ComboBox);
+		
+		JComboBox<String> serial2ComboBox = new JComboBox<String>(getSerialPorts());
+		//JComboBox<String> serial2ComboBox = new JComboBox<String>();
+		serial2ComboBox.setBounds(219, 23, 91, 22);
+		frame.getContentPane().add(serial2ComboBox);
+		
+		JComboBox<String> watcherComboBox = new JComboBox<String>(getSerialPorts());
+		//JComboBox<String> bypassComboBox = new JComboBox<String>();
+		watcherComboBox.setBounds(118, 60, 91, 22);
+		frame.getContentPane().add(watcherComboBox);
+		
+		JLabel lbl1stport = new JLabel("1st PORT");
+		lbl1stport.setBounds(12, 3, 73, 16);
+		frame.getContentPane().add(lbl1stport);
+		
+		JLabel lbl2ndport = new JLabel("2nd PORT");
+		lbl2ndport.setBounds(219, 3, 73, 16);
+		frame.getContentPane().add(lbl2ndport);
+		
+		JLabel lblbypass = new JLabel("watcher");
+		lblbypass.setBounds(115, 41, 56, 16);
+		frame.getContentPane().add(lblbypass);
+		
+		JButton Connect = new JButton("Connect");
+		Connect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String serial1Name = (String)serial1ComboBox.getSelectedItem();
+				String serial2Name = (String)serial2ComboBox.getSelectedItem();
+				String watcherName = (String)watcherComboBox.getSelectedItem();
+				
+				Connection conn = Connection.getInstance();
+				try {
+					conn.ConnectPorts(serial1Name, serial2Name, watcherName);
+				} catch (NoSuchPortException | PortInUseException | UnsupportedCommOperationException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		Connect.setBounds(118, 105, 97, 25);
+		frame.getContentPane().add(Connect);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private String[] getSerialPorts() {
+	
+		Enumeration portList = CommPortIdentifier.getPortIdentifiers();
+		ArrayList<CommPortIdentifier> ports = Collections.list(portList);
+		String[] ret = new String[ports.size()];
+		int i = 0;
+		for(CommPortIdentifier port : ports) {
+			ret[i++] = port.getName();
+		}
+		while(portList.hasMoreElements()){
+            CommPortIdentifier portId = (CommPortIdentifier) portList.nextElement();
+              if (portId.getPortType() == CommPortIdentifier.PORT_PARALLEL) {
+                   System.out.println(portId.getName());
+              }
+              else{
+                   System.out.println(portId.getName());
+              }
+              
+       }
+        return ret;
+	}
+}
